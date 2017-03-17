77bd9d7d8248d89f92c42e3b726684b7f6081c6b
Amitrajit Sarkar <aaiijmrtt@gmail.com>
2017-03-18 01:39:50 +0530

    fixing formats: music

--- code/music.py
+++ code/music.py
@@ -1,32 +1,35 @@
 import inspect
 import music21
 
-def checkmodule(obj, mod):
-	return isinstance(obj, tuple([cl for nm, cl in inspect.getmembers(mod, inspect.isclass)]))
-
+_debug_ = False
 music21.environment.set('musicxmlPath', '/usr/bin/musescore')
+checkmodule = lambda obj, mod: isinstance(obj, tuple([cl for nm, cl in inspect.getmembers(mod, inspect.isclass)]))
 
 def parse(xmlfile):
-	music, parsedmusic = music21.converter.parse(xmlfile, format = 'musicxml'), dict()
+	music, parsedmusic = music21.converter.parse(xmlfile, format = 'musicxml'), list()
 	for part in music:
 		if checkmodule(part, music21.metadata): metadata = part
 		elif checkmodule(part, music21.stream):
 			for measure in part:
 				if checkmodule(measure, music21.instrument):
 					instrument = measure.partName
-					parsedmusic[instrument] = {'bars': list()}
+					parsedmusic.append(list())
 				elif checkmodule(measure, music21.stream):
-					parsedmusic[instrument]['bars'].append(list())
+					parsedmusic[-1].append(list())
 					for note in measure:
-						if checkmodule(note, music21.clef): parsedmusic[instrument]['clef'] = note
-						elif checkmodule(note, music21.key): parsedmusic[instrument]['key'] = note
-						elif checkmodule(note, music21.meter): parsedmusic[instrument]['meter'] = note.ratioString
-						elif checkmodule(note, music21.bar): continue
+						if checkmodule(note, music21.clef): clef = note
+						elif checkmodule(note, music21.key): key = note
+						elif checkmodule(note, music21.meter): meter = note.ratioString
+						elif checkmodule(note, music21.bar): parsedmusic[-1].append(list())
 						elif checkmodule(note, music21.layout): continue
-						elif checkmodule(note, music21.note): parsedmusic[instrument]['bars'][-1].append((note.name, note.octave, note.offset, note.duration.quarterLength))
-						else: print 'type not recognized', type(note)
-				else: print 'type not recognized', type(measure)
-		else: print 'type not recognized', type(part)
+						elif checkmodule(note, music21.chord): continue
+						elif checkmodule(note, music21.note):
+							if note.isRest: continue
+							parsedmusic[-1][-1].append((note.name, note.octave, note.offset, note.duration.quarterLength, instrument, key, meter, clef))
+						elif _debug_: print 'type not recognized', type(note)
+				elif _debug_: print 'type not recognized', type(measure)
+		elif _debug_: print 'type not recognized', type(part)
+	if _debug_: music.show()
 	return parsedmusic
 
 if __name__ == '__main__':
